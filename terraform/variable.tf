07f79874bb2d84765cac37de26e536c87cc78986
gampasivakumar4422 <138592547+gampasivakumar4422@users.noreply.github.com>
2024-07-09 11:28:23 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,3 +1,22 @@
-variable "public_key" {
-  type = string
+variable "aws_region" {
+    type = string
 }
+variable "key_name" {
+    type = string
+}
+variable "instance_type" {
+    type = string
+}
+variable "ami" {
+    type = string
+}
+variable "vpc_security_group_id" {
+    type = string
+}
+variable "subnet_id" {
+    type = list
+}
+variable "vpc_id" {
+    type = string
+}
+
